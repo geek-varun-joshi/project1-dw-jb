ce3b1e98d2fedeecea3ed1a7765394498c80be53
Varun The-Geek Joshi <64461330+geek-varun-joshi@users.noreply.github.com>
2024-02-17 11:57:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! I have received your message and reach out to you shortly." ))
+  .then(response => alert("Thank you! I have received your message and I will reach out to you shortly." ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
